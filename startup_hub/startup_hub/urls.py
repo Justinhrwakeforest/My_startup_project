4c36ea102004d9945e08a2ca7a5c62a0342bbf18
Hruthik H R <101786141+Justinhrwakeforest@users.noreply.github.com>
2025-06-21 11:12:59 +0530

    Update urls.py

--- startup_hub/startup_hub/urls.py
+++ startup_hub/startup_hub/urls.py
@@ -1,7 +1,9 @@
-# In startup_hub/urls.py
+# In startup_hub/urls.py - Updated with media files support
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def api_stats(request):
     from apps.startups.models import Startup
@@ -22,3 +24,8 @@ urlpatterns = [
     path('api/jobs/', include('apps.jobs.urls')),
     path('api/stats/', api_stats, name='api_stats'),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
